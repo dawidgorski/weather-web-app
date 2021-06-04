b3dd8f3e6be168bd8a55f2b527fe1555d4c002bd
Dawid Górski <gorski.dawid.mail@gmail.com>
2021-06-04 22:07:30 +0200

    Refactor Controller

--- src/main/java/com/dawidgorski/WeatherApp/api/WeatherController.java
+++ src/main/java/com/dawidgorski/WeatherApp/api/WeatherController.java
@@ -1,28 +1,29 @@
 package com.dawidgorski.WeatherApp.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.IOException;
-
 @Controller
 public class WeatherController {
-    @Autowired
-    WeatherClient weatherClient;
+
+    WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
 
     @GetMapping("/weather")
     public String getWeather(Model model) {
-        model.addAttribute("weatherAtr", weatherClient);
+        model.addAttribute("weatherAtr", weatherService);
         return "currentWeather";
     }
 
     @PostMapping("/change_city")
-    public String changeCity(@ModelAttribute WeatherClient weatherClient) {
-        this.weatherClient.setCity(weatherClient.getCity());
+    public String changeCity(@ModelAttribute WeatherService weatherService) {
+        this.weatherService.setCity(weatherService.getCity());
         return "redirect:/weather";
     }
 }
